9ebbc131d04b3d503a0ade5429e76fdbb8581371
Vu Dinh Hiep <hiepvd@vmodev.com>
2022-10-21 11:33:25 +0700

    update project

---
+++ components/TexInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-form-item :label="label">
+    <el-input :type="type" v-model="ruleForm.pass" show-password></el-input>
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "label",
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+  },
+};
+</script>
+
+<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -15,10 +15,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ["~/assets/scss/app.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ["~/plugins/element-ui.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
